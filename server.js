a9e8e566f6673a1907d22a84513cef0dd4a36181
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-10-31 11:59:50 -0500

    setup server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Create instance of express app.
+var app = express();
+
+// Set the port of our application
+var PORT = process.env.PORT;
