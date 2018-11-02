644183c37b1bd9e63cf7e801f253f4db541f153b
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-11-01 22:58:46 -0500

    update things learned in class

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ var path = require("path");
 var app = express();
 
 // Set the port of our application
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080; 
 
-//Routes for API & HTML
-require("./app/routing/apiRoutes.js");
-require("./app/routing/htmlRoutes.js");
+//Routes for API & HTML files
+var apiRoutes = require("./app/routing/apiRoutes.js");
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
 
-// Start our server so that it can begin listening to client requests.
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(" ", htmlRoutes);
+app.use(" ", apiRoutes);
+
+// Start the server so that it can begin listening to client requests.
 app.listen(PORT, function () {
 
     // Log (server-side) when our server has started
